c68a4b48fee089fdbf7fcf87e35c14eb75d8da8b
Hack Reactor Students <accounts@hackreactor.com>
2014-07-08 00:31:09 -0700

    Reimplement Tab

--- buzzkill.js
+++ buzzkill.js
@@ -11,31 +11,61 @@ app.config(function($stateProvider, $urlRouterProvider) {
       url: "/main",
       templateUrl: "main.html"
     })
-    .state('order', {
-      url: "/order",
-      templateUrl: "order.html"
-    })
-    .state('pickup', {
-      url: "/pickup",
-      templateUrl: "pickup.html"
-    })
     .state('tab', {
       url: "/tab",
       templateUrl: "tab.html"
     })
+    .state('order', {
+      url: "/order",
+      templateUrl: "order.html"
+    })
     ;
 });
 
-app.controller("TabController", function($scope) {
-  $scope.name = 'Fred';
-  $scope.open = false;
+app.controller('mainController', function($scope, $firebase, FIREBASE_URL) {
+  // var drinksRef = new Firebase(FIREBASE_URL + "drinklist");
+  // var newDrinkRef = drinksRef.push();
 
-  $scope.openTab = function() {
-    $scope.open = true;
-    console.log("name: " + $scope.name);
-  };
-  $scope.closeTab = function() {
-    $scope.open = false;
-  };
+  // newDrinkRef.set({
+  //   name: 'Long Island Iced Tea',
+  //   image: 'http://static.squarespace.com/static/5176fdb5e4b083b631f31303/t/525eaec9e4b0880126d6821f/1381936843998/bigstock-Long-Island-Iced-Tea-43438963.jpg',
+  //   price: '$5 million'
+  // });
+
+  // console.dir(drinksRef);
+  // console.log("new drink rec: " + newDrinkRef.name());
+  // $scope.display = "mulligan";
 });
 
+
+app.controller("OrderController", function($scope, $firebase, FIREBASE_URL) {
+  // initialize drink list
+  var drinklistRef = new Firebase(FIREBASE_URL + 'drinklist');
+  $scope.drinklist = $firebase(drinklistRef);
+});
+
+app.controller("TabController", function($scope, $firebase, FIREBASE_URL) {
+  $scope.drinks = {};
+  $scope.open = true;
+
+  // initialize tab drinks
+  var drinksRef = new Firebase(FIREBASE_URL + 'drinks');
+  $scope.drinks = $firebase(drinksRef);
+  console.log($scope.drinks);
+
+  // TODO fix open/close tab code
+  // $scope.openTab = function() {
+  //   if ($scope.name !== null) {
+  //     var tabsRef = new Firebase(FIREBASE_URL + 'tabs');
+  //     tabsRef.$add($scope.tab);
+  //     $scope.open = true;
+  //     console.log("Tab opened: " + $scope.name);
+  //     console.log("=> id: " + newTabRef.name());
+  //   }
+  // };
+  // $scope.closeTab = function() {
+  //   var tabsRef = new Firebase(FIREBASE_URL + 'tabs');
+  //   tabsRef.$remove($scope.tab);
+  //   $scope.open = false;
+  // };
+});
